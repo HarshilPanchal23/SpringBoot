ec15a5c2e4b0b94a426f4cb5416d2c72d31d399c
Harshil84012 <harshilpanchal.softvan@gmail.com>
2024-11-11 16:38:36 +0530

    implement exception handling functionality

--- core/src/main/java/com/example/core/dto/ApiResponse.java
+++ core/src/main/java/com/example/core/dto/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.example.core.dto;
 
+import com.example.core.enums.CustomStatusEnum;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,8 +24,22 @@ public class ApiResponse {
     @JsonIgnore
     private HttpStatus httpStatus;
 
-
-    public ApiResponse(HttpStatus httpStatus, String value, UserRequestDto responseDTO) {
+    public ApiResponse(HttpStatus httpStatus, String message, Object data) {
+        this.httpStatus = httpStatus;
+        this.status = httpStatus.value();
+        this.message = message;
+        this.data = data;
     }
 
+    public ApiResponse(HttpStatus status, String message) {
+        this.status = status.value();
+        this.message = message;
+    }
+
+    public ApiResponse(CustomStatusEnum status, String message, Object data) {
+        this.status = status.getValue();
+        this.message = message;
+        this.httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        this.data = data;
+    }
 }
