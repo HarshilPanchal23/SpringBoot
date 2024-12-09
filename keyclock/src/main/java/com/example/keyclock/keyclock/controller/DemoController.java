2ecb1fd106fb218513d319895d973f59d7c35b3b
Harshil84012 <harshilpanchal.softvan@gmail.com>
2024-12-09 18:19:53 +0530

    add profiler

--- keyclock/src/main/java/com/example/keyclock/keyclock/controller/DemoController.java
+++ keyclock/src/main/java/com/example/keyclock/keyclock/controller/DemoController.java
@@ -1,4 +1,25 @@
 package com.example.keyclock.keyclock.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
 public class DemoController {
+
+
+    @GetMapping("hello-1")
+    @PreAuthorize("hasRole('client_user')")
+    public String hello1() {
+        return "hello from 1 ";
+    }
+
+    @GetMapping("hello-2")
+    @PreAuthorize("hasRole('client_admin')")
+    public String hello2() {
+        return "hello from 2 -  Admin";
+    }
+
 }
